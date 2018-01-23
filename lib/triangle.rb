aeaf7890aa7b51a9d9dc619397c14f33ce07fbc0
Ryan Manchester <ryanm17@gmail.com>
2018-01-23 03:58:13 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
     if @side_1 == @side_2 && @side_1 == @side_3
       :equilateral
     end
-    if @side_1 == @side_2 || @side_1 == @side_3
+    if @side_1 == @side_3 || @side_2 == @side_3
       :isosceles
     end
 
