f29e33b3017390f6f59e0db792ba281cee264fee
Ryan Manchester <ryanm17@gmail.com>
2018-01-23 03:56:01 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,6 @@
 class Triangle
   attr_accessor :side_1, :side_2, :side_3
-  def initialization(side_1, side_2, side_3)
+  def initialize(side_1, side_2, side_3)
     @side_1 = side_1
     @side_2 = side_2
     @side_3 = side_3
@@ -10,7 +10,7 @@ class Triangle
     if @side_1 == @side_2 && @side_1 == @side_3
       :equilateral
     end
-    
+
 
   end
 
