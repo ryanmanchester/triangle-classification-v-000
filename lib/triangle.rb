dc8feb64c9c5edbe3e6becb5c8886bc0a673bbe3
Ryan Manchester <ryanm17@gmail.com>
2018-01-23 04:47:12 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,10 +12,6 @@ class Triangle
       raise TriangleError
     elsif @side_1 + @side_2 < @side_3 || @side_1 + @side_3 == @side_2 || @side_2 + @side_3 < @side_1
       raise TriangleError
-    # elsif @side_1 + @side_2 == @side_3 || @side_1 + @side_3 == @side_2
-    #   raise TriangleError
-    # elsif @side_2 + @side_3 < @side_1
-    #   raise TriangleError
     end
     if @side_1 == @side_2 && @side_1 == @side_3
       :equilateral
