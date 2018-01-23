f4d9103844a52f61c54e0f3d1f7069d6c0a8d82e
Ryan Manchester <ryanm17@gmail.com>
2018-01-23 04:13:24 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,10 +15,11 @@ class Triangle
 
     elsif @side_1 != @side_2 && @side_1 != @side_3
       :scalene
+
+    else 
+      raise TriangleError
     end
-   if @side_1 <= 0 || @side_2 <= 0 || @side_3 <= 0
-     raise TriangleError
-   end
+   
 
   end
 
